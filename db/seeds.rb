f91e8dd67bbcbdabfd651270d87a36c0af64e4a5
Shujaat Azim <shujaat.azim@gmail.com>
2020-11-11 20:25:22 -0500

    Seed data for separate list.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 print "Seeding..."
 
-list01 = List.create(name: "Shu's List")
+list01 = List.create(name: "Productivity List")
+list02 = List.create(name: "Maybe List")
 
 item01 = Item.create(list_id: list01.id, content: "Networking")
 item02 = Item.create(list_id: list01.id, content: "Algorithms")
 item03 = Item.create(list_id: list01.id, content: "Project")
 item04 = Item.create(list_id: list01.id, content: "Learning")
+
+item05 = Item.create(list_id: list02.id, content: "Chess Game")
+item06 = Item.create(list_id: list02.id, content: "Japanese Lesson")
 
 print "Done!"
