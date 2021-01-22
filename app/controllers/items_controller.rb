100b55bb96a9de1cac5bf4c390c9937f865ec1d0
Shujaat Azim <shujaat.azim@gmail.com>
2021-01-21 19:23:15 -0500

    Some errors, removed some parentheses.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,25 +2,25 @@ class ItemsController < ApplicationController
 
 	def index
 		@items = Item.all
-		render json: ItemSerializer.new(@items).to_serialized_json()
+		render json: ItemSerializer.new(@items).to_serialized_json
 	end
 
 	def show
 		@item = Item.find(params[:id])
-		render json: ItemSerializer.new(@item).to_serialized_json()
+		render json: ItemSerializer.new(@item).to_serialized_json
 	end
 
 	def create
 		@item = Item.new(item_params)
 		@item.save
-		render json: ItemSerializer.new(@item).to_serialized_json()
+		# render json: ItemSerializer.new(@item).to_serialized_json
 	end
 
 	def update
 		@item = Item.find(params[:id])
 		@item.update(content: params[:content], complete: params[:complete])
 		@item.save
-		render json: ItemSerializer.new(@item).to_serialized_json()
+		render json: ItemSerializer.new(@item).to_serialized_json
 	end
 
 	def destroy
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,25 +2,25 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
-    render json: ListSerializer.new(@lists).to_serialized_json()
+    render json: ListSerializer.new(@lists).to_serialized_json
   end
 
   def show
     @list = List.find(params[:id])
-    render json: ListSerializer.new(@list).to_serialized_json()
+    render json: ListSerializer.new(@list).to_serialized_json
   end
   
   def create
     @list = List.new(list_params)
     @list.save
-    render json: ListSerializer.new(@list).to_serialized_json()
+    # render json: ListSerializer.new(@list).to_serialized_json
   end
 
   def update
     @list = List.find(params[:id])
     @list.update(name: params[:name])
     @list.save
-    render json: ListSerializer.new(@list).to_serialized_json()
+    render json: ListSerializer.new(@list).to_serialized_json
   end
 
   def destroy
