4bad99e3bb71353f5630eaaf9531e8688f7f1186
Shujaat Azim <shujaat.azim@gmail.com>
2021-03-26 17:04:43 -0400

    Built out user controller for authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ApplicationRecord
+
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
 
-  
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      payload = { user_id: @user.id }
+      token = create_token(payload)
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
 end
