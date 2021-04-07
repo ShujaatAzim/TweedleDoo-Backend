384162d1baecfec8b9ba2bcae13ff26e8afb5250
Shujaat Azim <shujaat.azim@gmail.com>
2021-04-07 19:53:50 -0400

    Added user serializer for future use.

---
+++ app/services/user_serializer.rb
@@ -0,0 +1,14 @@
+class UserSerializer
+
+  def initialize(user_object)
+    @user = user_object
+  end
+
+  def to_serialized_json
+    options = {
+      except: [:created_at, :updated_at]
+    }
+    @user.to_json(options)
+  end
+
+end
