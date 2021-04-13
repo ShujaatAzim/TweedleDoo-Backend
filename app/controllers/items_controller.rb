a4cf566b8ca6a411940e0cffb3df76ec5dc081e0
Shujaat Azim <shujaat.azim@gmail.com>
2021-04-12 20:07:02 -0400

    Changed auth route and method from create to login to make more sense. Added skip to items controller so that I can still access lists and items. Not able to log in with seed data, so will have to fix.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
 
+	skip_before_action :authorized #only for dev purposes
+
 	def index
 		@items = Item.all
 		render json: ItemSerializer.new(@items).to_serialized_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :create]
-  post '/login', to: 'authentication#create'
+  post '/login', to: 'authentication#login'
   get '/profile', to: 'users#profile'
   resources :lists
   resources :items
