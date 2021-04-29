3ca8be9bbd438062e42b5a25dba0e7ca2c639a75
Shujaat Azim <shujaat.azim@gmail.com>
2021-04-29 15:39:11 -0400

    Added tons more seed data for demo purposes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,31 @@
 print "Seeding..."
 
-user01 = User.create(username: "shu", password: "123")
-user02 = User.create(username: "test", password: "124")
+user01 = User.create(username: "Shu", password: "123")
+user02 = User.create(username: "Test", password: "124")
 
-list01 = List.create(name: "Shu's List 1", user_id: user01.id)
-list02 = List.create(name: "Shu's List 2", user_id: user01.id)
-list03 = List.create(name: "Test's List 1", user_id: user02.id)
-list04 = List.create(name: "Test's List 2", user_id: user02.id)
+list01 = List.create(name: "Shu's Productivity List", user_id: user01.id)
+list02 = List.create(name: "Shu's Fun List", user_id: user01.id)
+list03 = List.create(name: "Test's Tasks", user_id: user02.id)
+list04 = List.create(name: "Test's Call List", user_id: user02.id)
 
-item01 = Item.create(list_id: list01.id, content: "Networking", complete: true)
+item01 = Item.create(list_id: list01.id, content: "Networking", complete: false)
 item02 = Item.create(list_id: list01.id, content: "Algorithms", complete: false)
+item03 = Item.create(list_id: list01.id, content: "Project", complete: false)
+item04 = Item.create(list_id: list01.id, content: "Learning", complete: false)
 
-item03 = Item.create(list_id: list02.id, content: "Project", complete: false)
-item04 = Item.create(list_id: list02.id, content: "Learning", complete: false)
+item05 = Item.create(list_id: list02.id, content: "Chess Game", complete: false)
+item06 = Item.create(list_id: list02.id, content: "Japanese Lesson", complete: false)
+item07 = Item.create(list_id: list02.id, content: "Travel Planning", complete: false)
+item08 = Item.create(list_id: list02.id, content: "Japanese Lesson", complete: false)
 
-item05 = Item.create(list_id: list03.id, content: "Chess Game", complete: false)
-item06 = Item.create(list_id: list03.id, content: "Japanese Lesson", complete: true)
+item09 = Item.create(list_id: list03.id, content: "Passport Renewal", complete: false)
+item10 = Item.create(list_id: list03.id, content: "Grocery Shopping", complete: false)
+item11 = Item.create(list_id: list03.id, content: "Get Car Serviced", complete: false)
 
-item07 = Item.create(list_id: list04.id, content: "Test item 1", complete: true)
-item08 = Item.create(list_id: list04.id, content: "Test item 2", complete: false)
+item12 = Item.create(list_id: list04.id, content: "Call Work", complete: false)
+item13 = Item.create(list_id: list04.id, content: "Call Friends", complete: false)
+item14 = Item.create(list_id: list04.id, content: "Call Agent", complete: false)
+item15 = Item.create(list_id: list04.id, content: "Call Customer Service", complete: false)
+
 
 print "Done!"
