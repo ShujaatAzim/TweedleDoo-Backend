bc09d558b0ca31eb9880d44b3b6acb9168589fd7
Shujaat Azim <shujaat.azim@gmail.com>
2025-05-06 02:15:58 +0900

    Fix login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
 
 
   def secret
-    secret = ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base
+    ENV['SECRET_KEY_BASE'] || Rails.application.credentials.secret_key_base
   end
 
 
